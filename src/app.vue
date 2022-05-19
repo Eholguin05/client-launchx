16c3608aea1021cc1b6552e5d805e3d9fccf2f99
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-05-19 00:03:09 -0500

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-light bg-white">
+      <router-link to="/" class="navbar-brand">LaunchX</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/explorers" class="nav-link">Explorers</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Agregar Explorer</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/missionCommanders" class="nav-link">Mission Commanders</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/addMissionCommander" class="nav-link">Agregar Mission Commander</router-link>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "app"
+};
+</script>
